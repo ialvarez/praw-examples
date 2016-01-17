cf3b2053896815dad50477e52ad81a2b5f31f248
Ignacio Alvarez <ignacio.alvarezbarrio@gmail.com>
2016-01-17 13:04:55 +0100

    new small example

---
+++ hello_oauth/app_config_data.py
@@ -0,0 +1,3 @@
+CLIENT_ID = 'ba5De5oZ0xk3_w'
+CLIENT_SECRET = 'WiHCIJH94JZOIhi0b81Nxgbfte8'
+REDIRECT_URI = 'http://127.0.0.1:65010/authorize_callback'
---
+++ hello_oauth/server.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+import praw
+import app_config_data
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    link_no_refresh = r.get_authorize_url('UniqueKey')
+    link_refresh = r.get_authorize_url('DifferentUniqueKey',
+                                       refreshable=True)
+    link_no_refresh = "<a href=%s>link</a>" % link_no_refresh
+    link_refresh = "<a href=%s>link</a>" % link_refresh
+    text = "First link. Not refreshable %s</br></br>" % link_no_refresh
+    text += "Second link. Refreshable %s</br></br>" % link_refresh
+    return text
+
+
+@app.route('/authorize_callback')
+def authorized():
+    state = request.args.get('state', '')
+    code = request.args.get('code', '')
+    info = r.get_access_information(code)
+    user = r.get_me()
+    variables_text = "State=%s, code=%s, info=%s." % (state, code,
+                                                      str(info))
+    text = 'You are %s and have %u link karma.' % (user.name,
+                                                   user.link_karma)
+    back_link = "<a href='/'>Try again</a>"
+    return variables_text + '</br></br>' + text + '</br></br>' + back_link
+
+if __name__ == '__main__':
+    r = praw.Reddit('praw-examples/hello_oauth')
+    r.set_oauth_app_info(app_config_data.CLIENT_ID, app_config_data.CLIENT_SECRET, app_config_data.REDIRECT_URI)
+    app.run(debug=True, port=65010)
