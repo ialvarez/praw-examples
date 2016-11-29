8b36a518989e0318369658c2de16644fd65f3971
Ignacio Alvarez <ignacio.alvarezbarrio@gmail.com>
2016-11-29 21:54:11 +0100

    Adding client information as it was already public in another place

--- hello_world/main.py
+++ hello_world/main.py
@@ -1,9 +1,7 @@
 import praw
 
-
-# Here you need to add your information
-client_id = ''
-client_secret = ''
+client_id = 'ba5De5oZ0xk3_w'
+client_secret = 'WiHCIJH94JZOIhi0b81Nxgbfte8'
 
 r = praw.Reddit(user_agent='hello_world',
                 client_id=client_id,
