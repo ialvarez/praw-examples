eef111294f22ce405d7c1ab58225dda245ad250c
Ignacio Alvarez <ignacio.alvarezbarrio@gmail.com>
2016-11-29 21:52:21 +0100

    adapting to the new version of the library

--- hello_world/main.py
+++ hello_world/main.py
@@ -1,5 +1,13 @@
 import praw
 
-r = praw.Reddit(user_agent='hello_world')
-submissions = r.get_subreddit('opensource').get_hot(limit=5)
-print [str(x) for x in submissions]
+
+# Here you need to add your information
+client_id = ''
+client_secret = ''
+
+r = praw.Reddit(user_agent='hello_world',
+                client_id=client_id,
+                client_secret=client_secret)
+submissions = r.subreddit('opensource').hot(limit=5)
+
+print([str(x.title) for x in submissions])
